6458b29a08db603af8d147b44feae613dbd952ff
Mustafa Nomair <mnomair01@gmail.com>
2025-07-23 12:50:00 -0700

    fix: added requirements dep. & readme documentation

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 class Config:
-    GEOCODIO_API_KEY=os.getenv('GEOCODIO_API_KEY')
+    SPOONACULAR_API_KEY=os.getenv('SPOONACULAR_API_KEY')
     GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
