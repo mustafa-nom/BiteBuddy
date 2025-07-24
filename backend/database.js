38c771f5ecc546d604e4abd835d4794c769f5922
Abtin Khashayar <akhashayar26@berkeley.edu>
2025-07-24 00:24:00 -0700

    removed unecessary code

--- backend/database.js
+++ backend/database.js
@@ -9,21 +9,6 @@ import {
   updateDoc, deleteField, arrayRemove,
   writeBatch, 
 } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDF6015nfRRKcOMGC6oJUJhTzB2zMNi8io",
-//   authDomain: "bitebuddy-625fd.firebaseapp.com",
-//   projectId: "bitebuddy-625fd",
-//   storageBucket: "bitebuddy-625fd.firebasestorage.app",
-//   messagingSenderId: "252168267480",
-//   appId: "1:252168267480:web:ee077f8671632ab62719ad",
-//   measurementId: "G-617XYRJPHV"
-// };
-
 
 const firebaseConfig = {
   apiKey: process.env.apiKey,
