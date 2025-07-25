fd6966c40327d06b53dd7877d8c27f3553217e0a
Abtin Khashayar <akhashayar26@berkeley.edu>
2025-07-25 10:10:26 -0700

    Created some authentication

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -9,12 +9,28 @@ export default function Login({ onLogin }) {
     const handleSubmit = async (e) => {
         e.preventDefault();
         // replace with real user authorization later, right now just checks that there is something entered
-        if (username && password) {
-            await addUserLogin(username);
-            onLogin(); // triggers the setIsLoggedIn in App.js
+        if (!username || !password) {
+            // await addUserLogin(username);
+            // onLogin(); // triggers the setIsLoggedIn in App.js
+            return alert("Please enter a username and password!");
         }
-        else {
-            alert('Please enter username and password.');
+        try {
+            const user = await getUserData(username);
+            if (user) {
+                if (user.password === password) {
+
+                    onLogin(username); // triggers the setIsLoggedIn in App.js
+                } else {
+                    alert('Incorrect password. Please try again.');
+                }
+            }
+            else {
+                await addUserLogin(username); // just for debugging, but we need to put this in the sign up page!
+                alert("User does not exit. Please sign up first!");
+            }
+        }
+        catch (err){
+            console.error("Error logging in:", err);
         }
     };
 
