aeeed4c7b20bca74e166b826c624ae52dfa3552c
Mustafa Nomair <mnomair01@gmail.com>
2025-07-28 10:08:44 -0700

    fix: merging conflicts v2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,26 @@ import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 
 import './App.css';
 
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import Recipe from './pages/Recipes'
-import Fridge from './pages/Fridge'
-
-import Mealplan from './pages/Mealplan'
-import Navbar from './components/Navbar'
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Recipe from './pages/Recipes';
+import Fridge from './pages/Fridge';
+import Signup from './pages/Signup';
+import RecipeDetails from './pages/RecipeDetails';
+import Mealplan from './pages/Mealplan';
+import Navbar from './components/Navbar';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [currentUser, setCurrentUser] = useState(null);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleLogin = (username) => {
     setCurrentUser(username);
     setIsLoggedIn(true);
   };
+
   const handleLogout = () => {
     setCurrentUser(null);
     setIsLoggedIn(false);
@@ -41,10 +43,12 @@ function App() {
               )
             }
           />
+
           <Route
             path="/signup"
-            element={<Signup onSignupSuccess={() => navigate("/")} />}
+            element={<Signup onSignupSuccess={() => navigate('/')} />}
           />
+
           <Route
             path="/dashboard"
             element={
@@ -56,32 +60,33 @@ function App() {
             }
           />
 
-          <Route path="/recipes" element={<Recipe />} />
-
           <Route
-            path="/fridge"
-            element={isLoggedIn ? <Fridge username={currentUser} /> : <Navigate to="/" />}
-          />
-
-          <Route
-            path = "/recipedetails"
-            element = {
-              <RecipeDetails/>
+            path="/recipes"
+            element={
+              isLoggedIn ? <Recipe /> : <Navigate to="/" />
             }
           />
 
-          
-          <Route 
-            path="/recipe/:recipeId" 
-            element={<RecipeDetails />} 
+          <Route
+            path="/fridge"
+            element={
+              isLoggedIn ? <Fridge username={currentUser} /> : <Navigate to="/" />
+            }
           />
-    
-            <Route
-              path = "/mealplan"
-              element = {
-                <Mealplan/>
-              }
-            />
+
+          <Route
+            path="/recipe/:recipeId"
+            element={
+              isLoggedIn ? <RecipeDetails /> : <Navigate to="/" />
+            }
+          />
+
+          <Route
+            path="/mealplan"
+            element={
+              isLoggedIn ? <Mealplan /> : <Navigate to="/" />
+            }
+          />
         </Routes>
       </div>
     </>
